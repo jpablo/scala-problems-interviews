419e48bc923475aeb077a587d569825be30fd178
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-10-11 12:10:10 -0700

    refactoring

--- src/main/scala/puzzles/leetCode/easyProblems/p14_LongestCommonPrefix.scala
+++ src/main/scala/puzzles/leetCode/easyProblems/p14_LongestCommonPrefix.scala
@@ -1,11 +1,13 @@
-package puzzles.leetCode
+package puzzles.leetCode.easyProblems
+
+import scala.annotation.tailrec
 
 // 14
 // https://leetcode.com/problems/longest-common-prefix/
 
 
 // Write a function to find the longest common prefix string amongst an array of strings.
-// 
+//
 // If there is no common prefix, return an empty string "".
 
 
@@ -13,33 +15,36 @@ type Pos = Int
 
 // Idea:
 // - Compare each char of the *first* word against all others
-def longestCommonPrefix(strs: Array[String]): String = {
-  
-  val head = strs(0)
 
-  def loop(i: Pos = 0, prefix: String = ""): String = {
-    if (i < head.length && commonAtPosition(head(i), i))
+def longestCommonPrefix(words: Array[String]): String = {
+
+  val firstWord = words(0)
+
+  @tailrec
+  def loop(i: Pos = 0, acc: String = ""): String = {
+    if (i < firstWord.length && commonAtPosition(firstWord(i), i, 0))
       // if the char at pos `i` (in head) is the same in all words,
-      // add it to the running prefix 
-      loop(i + 1, prefix + head(i))
+      // add it to the running prefix
+      loop(i + 1, acc + firstWord(i))
     else
-      prefix        
+      acc
   }
-  
-  def commonAtPosition(c: Char, cPos: Pos, j: Int = 0): Boolean =
-    if (j < strs.length)
-      if (cPos < strs(j).length && c == strs(j)(cPos))
+
+  @tailrec
+  def commonAtPosition(c: Char, cPos: Pos, wordPos: Int = 0): Boolean =
+    if (wordPos < words.length)
+      if (cPos < words(wordPos).length && c == words(wordPos)(cPos))
         // chars are the same for current word, go to next word
-        commonAtPosition(c, cPos, j + 1)
-      else 
+        commonAtPosition(c, cPos, wordPos + 1)
+      else
         false
     else
       true
 
-  loop()
+  loop(i = 0)
 }
 
-@main def main14 =
+@main def main14(): Unit =
   assert(longestCommonPrefix(Array("flower","flow","flight")) == "fl")
   assert(longestCommonPrefix(Array("dog","racecar","car")) == "")
   println("Ok")
