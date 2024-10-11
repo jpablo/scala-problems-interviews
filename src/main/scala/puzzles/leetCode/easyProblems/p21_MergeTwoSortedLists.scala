9099b90f15797955ce0611c85b989d79fe0adb19
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-10-11 13:05:37 -0700

    refactoring

--- src/main/scala/puzzles/leetCode/easyProblems/p21_MergeTwoSortedLists.scala
+++ src/main/scala/puzzles/leetCode/easyProblems/p21_MergeTwoSortedLists.scala
@@ -1,39 +1,39 @@
 package puzzles.leetCode.easyProblems
 
+import upickle.default.writeJs
+
 import scala.language.unsafeNulls
 
 // 21
 // https://leetcode.com/problems/merge-two-sorted-lists/
 
-
+// This is given in the problem description
 class ListNode(
-  var x   : Int = 0, 
-  var next: ListNode = null
+    var x:    Int = 0,
+    var next: ListNode = null
 ):
   //  var next: ListNode = _next
   //  var x: Int = _x
 
-   override def toString =
+  override def toString =
     s"ListNode($x, $next)"
 
-
+  def toList: List[Int] =
+    x :: (if next == null then Nil else next.toList)
 
 def mergeTwoLists(a: ListNode, b: ListNode): ListNode =
-  (a, b) match {
+  (a, b) match
     case (null, b) => b
     case (a, null) => a
-    case _ => 
-      if (a.x < b.x) {
-      // a.head :: merge(a.tail, b)
-      a.next = mergeTwoLists(a.next, b)
-      a
-    } else {
-      b.next = mergeTwoLists(b.next, a)
-      b
-    }
-  }
+    case _ =>
+      if a.x < b.x then
+        a.next = mergeTwoLists(a.next, b)
+        a
+      else
+        b.next = mergeTwoLists(b.next, a)
+        b
 
-@main def main21 =
+@main def main21(): Unit =
   val n1 = ListNode(1, ListNode(2, ListNode(4)))
   val n2 = ListNode(1, ListNode(3, ListNode(4)))
-  println(mergeTwoLists(n1, n2))
+  println(writeJs(mergeTwoLists(n1, n2).toList))
