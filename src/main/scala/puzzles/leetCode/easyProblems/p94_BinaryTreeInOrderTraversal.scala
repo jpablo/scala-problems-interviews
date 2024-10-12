5171d8805670fbecfa045aa0691610b9ab485bb1
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-10-12 10:35:28 -0700

    simplification

--- src/main/scala/puzzles/leetCode/easyProblems/p94_BinaryTreeInOrderTraversal.scala
+++ src/main/scala/puzzles/leetCode/easyProblems/p94_BinaryTreeInOrderTraversal.scala
@@ -1,5 +1,6 @@
-package puzzles.leetCode
+package puzzles.leetCode.easyProblems
 
+import puzzles.leetCode.TreeNode
 import scala.annotation.tailrec
 
 // 94
@@ -7,14 +8,13 @@ import scala.annotation.tailrec
 
 /*
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
-*/
+ */
 
-/**
-  * preorder: root, left, right
-  * 
+/** preorder: root, left, right
+  *
   * Idea:
-    - Stack of pending nodes
-    - use value when visiting node
+  * \- Stack of pending nodes
+  * \- use value when visiting node
   */
 def preorderTraversal(root: TreeNode): List[Int] = {
   @tailrec
@@ -22,19 +22,17 @@ def preorderTraversal(root: TreeNode): List[Int] = {
     (root, pending) match {
       case (n: TreeNode, _) => loop(n.left, n.right :: pending, use(n.value, result), use)
       case (null, r :: t)   => loop(r, t, result, use)
-      case (null, Nil   )   => result
+      case (null, Nil)      => result
     }
 
   val values = loop(root, List.empty, List.empty[Int], _ :: _)
   values.reverse
 }
 
-
-/**
-  * inorder: left, root, right
+/** inorder: left, root, right
   *
   * Idea:
-  *  - Stack of pending nodes + value
+  *   - Stack of pending nodes + value
   */
 def inorderTraversal(root: TreeNode): List[Int] = {
   @tailrec
@@ -45,39 +43,35 @@ def inorderTraversal(root: TreeNode): List[Int] = {
       case (null, Nil)             => result
     }
 
-  val values = loop(root, List.empty, List.empty[Int],  _ :: _)
+  val values = loop(root, List.empty, List.empty[Int], _ :: _)
   values.reverse
 }
 
-
-
-/**
-  * postorder: left, right, root
+/** postorder: left, right, root
   */
 def postorderTraversal(root: TreeNode): List[Int] = {
   @tailrec
   def loop[B](root: TreeNode | Null, pending: List[Int | TreeNode | Null], result: B, use: (Int, B) => B): B =
     (root, pending) match {
-      case (n: TreeNode, _) => 
+      case (n: TreeNode, _) =>
         loop(n.left, n.right :: n.value :: pending, result, use)
 
-      case (null, h :: t) => 
-        h match 
+      case (null, h :: t) =>
+        h match
           case value: Int  => loop(null, t, use(value, result), use)
-          case r: TreeNode => loop(r   , t, result,             use)
-          case null        => loop(null, t, result,             use)
-        
-      case (null, Nil) => 
+          case r: TreeNode => loop(r, t, result, use)
+          case null        => loop(null, t, result, use)
+
+      case (null, Nil) =>
         result
     }
 
-  val values = loop(root, List.empty, List.empty[Int],  _ :: _)
+  val values = loop(root, List.empty, List.empty[Int], _ :: _)
   values.reverse
 }
 
-@main def main94 =
+@main def main94(): Unit =
   val t = TreeNode(1, null, TreeNode(2, TreeNode(3)))
   println(preorderTraversal(t))
   println(inorderTraversal(t))
   println(postorderTraversal(t))
-
